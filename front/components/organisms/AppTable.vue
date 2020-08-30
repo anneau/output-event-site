3f9605d8a3548f3a86d4e56a76ee20484db38e9f
“halmuda” <halmuda6231@gmail.com>
2020-08-30 15:04:51 +0900

    add: #15 AppTableコンポーネントの追加

---
+++ front/components/organisms/AppTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>順位</th>
+        <th>チーム名</th>
+        <th>ポイント</th>
+        <th>LGTM数</th>
+        <th>投稿数</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(team, index) in teams" :key="team.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ team.name }}</td>
+        <td>{{ team.points_count }}</td>
+        <td>{{ team.items_count }}</td>
+        <td>{{ team.likes_count }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    teams: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
