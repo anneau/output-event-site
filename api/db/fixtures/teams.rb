8440586bd2909d6a539f0c1820f42259d8c02771
hatahata7757 <syouki100241@gmail.com>
2020-08-30 12:26:38 +0900

    add: #18 seedデータの作成

---
+++ api/db/fixtures/teams.rb
@@ -0,0 +1,4 @@
+Team.seed(:id,
+          { id: 1, name: 'TeamA', points_count: 11, likes_count: 1, items_count: 1 },
+          { id: 2, name: 'TeamB', points_count: 22, likes_count: 2, items_count: 2 },
+          { id: 3, name: 'TeamC', points_count: 33, likes_count: 3, items_count: 3 })
