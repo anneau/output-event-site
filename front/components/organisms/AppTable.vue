db17c8dde5b888313b8fbd8b437956ac4c9540f0
“halmuda” <halmuda6231@gmail.com>
2020-08-30 21:59:31 +0900

    fix: #15 順位の計算方法を変更

--- front/components/organisms/AppTable.vue
+++ front/components/organisms/AppTable.vue
@@ -11,7 +11,7 @@
     </thead>
     <tbody>
       <tr v-for="(team, index) in teams" :key="team.id">
-        <td>{{ rank(index, teams) }}</td>
+        <td>{{ rank(teams)[index] }}</td>
         <td>{{ team.name }}</td>
         <td>{{ team.points_count }}</td>
         <td>{{ team.items_count }}</td>
@@ -29,16 +29,13 @@ export default {
       required: true,
     },
   },
+
   methods: {
-    rank(index, teams) {
+    rank(teams) {
       // TODO: もっと綺麗に書ける気がする
-      if (index === 0) {
-        return 1
-      } else if (teams[index].points_count === teams[index - 1].points_count) {
-        return index
-      } else {
-        return index + 1
-      }
+      const points = teams.map((team) => team.points_count)
+      const sorted = points.slice().sort((before, after) => after - before)
+      return sorted.slice().map((rank) => sorted.indexOf(rank) + 1)
     },
   },
 }
