c2407472a4e555b5a3abcc89ea7eb8d704af1aec
hatahata7757 <syouki100241@gmail.com>
2020-08-30 11:17:00 +0900

    fix: #14 イベント終了日程までのQiita記事を取得するように修正

--- api/lib/assets/qiita.rb
+++ api/lib/assets/qiita.rb
@@ -8,21 +8,17 @@ module Assets::Qiita
     # 指定したユーザーの投稿一覧を返すQiitaAPIのエンドポイント
     uri = URI.parse("https://qiita.com/api/v2/users/#{user_name}/items")
     res = Net::HTTP.get_response(uri)
-
     # JSONをパースし投稿一覧を取得
     items = JSON.parse(res.body)
 
-    likes_count = 0
-    items_count = 0
-
-    # アウトプット企画実施日
-    event_date = Date.parse('2020-09-01')
+    likes_count = items_count = 0
+    event_start_date = Date.parse('2020-09-01')
+    event_end_date = Date.parse('2020-09-30')
 
     items.each do |item|
       created_at = Date.parse(item['created_at'])
-
-      # アウトプット企画実施日以降の記事投稿数とLGTM数をカウントする
-      if created_at >= event_date
+      # アウトプット企画実施期間中の記事投稿数とLGTM数をカウントする
+      if created_at >= event_start_date && created_at <= event_end_date
         likes_count += item['likes_count'].to_i
         items_count += 1
       end
